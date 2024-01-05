badbc41ed84012d954e6c47c8252052d06e921dc
Mwiyeria Kelvin <118157498+Dr-mkelvo@users.noreply.github.com>
2024-01-05 15:57:46 +0300

    Add files via upload
    First commit

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from "react"
+import Contact from "./Contact"
+import Navbar from "./Navbar"
+
+function App() {
+    return (
+        <>
+        <Navbar/>
+        <div className="contacts">
+            <Contact 
+                img="./images/mr-whiskerson.png" 
+                name="Charles Koome" 
+                phone="254 727349902"
+                email="chalo@mandeferoper.com"
+                role="Cyber Security"
+            />
+            <Contact 
+                img="./images/fluffykins.png"
+                name="Mwiyeria Kelvin"
+                phone="254 758594044"
+                email="mkelvo@mandeferoper.com"
+                role="Software Engineer"
+            />
+            <Contact 
+                img="./images/felix.png"
+                name="Rahma Said"
+                phone="254 700224304"
+                email="rehe@mandeferoper.com"
+                role="Network Engineer"
+            />
+            <Contact 
+                img="./images/pumpkin.png"
+                name="Chris Mukunya"
+                phone="254 703614501"
+                email="chris@mandeferoper.com"
+                role="Cyber Security"
+            />
+        </div>
+        </>
+    )
+}
+
+export default App
---
+++ Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <h3 className="nav--logo_text">Who Are We?</h3>
+            <h4 className="nav--title">Mandeferopers</h4>
+        </nav>
+    )
+}
